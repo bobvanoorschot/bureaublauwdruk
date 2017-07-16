a341f28d418d9a86486f15245ebb72b0c5162ccb
Bob van Oorschot <bvanoorschot@bukazu.com>
2017-07-16 14:21:16 +0200

    Continuing on the frontpage development

--- front-page.php
+++ front-page.php
@@ -4,16 +4,21 @@
   $loop = new WP_Query( array( 'post_type' => 'project', 'ignore_sticky_posts' => 1 ) );
   if ( $loop->have_posts() ) :
       while ( $loop->have_posts() ) : $loop->the_post(); ?>
-          <div class="pindex">
-              <?php if ( has_post_thumbnail() ) { ?>
-                  <div class="pimage">
-                      <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
+      <?php if ( has_post_thumbnail() ) { ?>
+          <div class="project" style="background-image: url('<?php get_the_post_thumbnail_url() ?>')">
+                  <div class="project-image">
+                      <a href="<?php the_permalink(); ?>">
+
+                        <div class="project-title">
+                          <h2><?php echo get_the_title(); ?></h2>
+                          <?php echo get_the_post_thumbnail_url(); ?>
+                        </div>
+                      </a>
                   </div>
-              <?php } ?>
-              <div class="ptitle">
-                  <h2><?php echo get_the_title(); ?></h2>
-              </div>
           </div>
+          <?php } else {
+            echo "Project mist foto";
+          } ?>
       <?php endwhile;
   endif;
   wp_reset_postdata();
